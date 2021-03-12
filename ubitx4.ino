c9032ad2d2f6bee3ad9c112d4461cecca2c2c7d6
Adrian Chadd <adrian.chadd@fb.com>
2021-03-11 16:29:10 -0800

    Migrate the defaults for the ubitx4 board into ubitx4_defaults.h!

--- ubitx4.ino
+++ ubitx4.ino
@@ -35,6 +35,7 @@
 
 #include "ubitx4_eeprom_defs.h"
 #include "ubitx4_pin_config.h"
+#include "ubitx4_defaults.h"
 
 /**
     The main chip which generates upto three oscillators of various frequencies in the
@@ -66,46 +67,10 @@ char c[30], b[30];
 char printBuff[2][31];  //mirrors what is showing on the two lines of the display
 int count = 0;          //to generally count ticks, loops, etc
 
-// Delay when switching between TX and RX.
-// This prevents spurious bursts when switching RX->TX.
-#define TX_DELAY_OSC_OFF 15  // wait 15ms after turning off si5351 for it to be off off
-#define TX_DELAY_ENABLE 30 // wait 30ms after enabling PTT to let things settle.
-
-/**
- * The uBITX is an upconnversion transceiver. The first IF is at 45 MHz.
- * The first IF frequency is not exactly at 45 Mhz but about 5 khz lower,
- * this shift is due to the loading on the 45 Mhz crystal filter by the matching
- * L-network used on it's either sides.
- * The first oscillator works between 48 Mhz and 75 MHz. The signal is subtracted
- * from the first oscillator to arriive at 45 Mhz IF. Thus, it is inverted : LSB becomes USB
- * and USB becomes LSB.
- * The second IF of 12 Mhz has a ladder crystal filter. If a second oscillator is used at 
- * 57 Mhz, the signal is subtracted FROM the oscillator, inverting a second time, and arrives 
- * at the 12 Mhz ladder filter thus doouble inversion, keeps the sidebands as they originally were.
- * If the second oscillator is at 33 Mhz, the oscilaltor is subtracated from the signal, 
- * thus keeping the signal's sidebands inverted. The USB will become LSB.
- * We use this technique to switch sidebands. This is to avoid placing the lsbCarrier close to
- * 12 MHz where its fifth harmonic beats with the arduino's 16 Mhz oscillator's fourth harmonic
- */
-
-// the second oscillator should ideally be at 57 MHz, however, the crystal filter's center frequency 
-// is shifted down a little due to the loading from the impedance matching L-networks on either sides
-#define SECOND_OSC_USB (56995000l)
-#define SECOND_OSC_LSB (32995000l) 
- 
-
-//these are the two default USB and LSB frequencies. The best frequencies depend upon your individual taste and filter shape
-#define INIT_USB_FREQ   (11996500l)
-// limits the tuning and working range of the ubitx between 3 MHz and 30 MHz
-#define LOWEST_FREQ   (100000l)
-#define HIGHEST_FREQ (30000000l)
-
 //we directly generate the CW by programmin the Si5351 to the cw tx frequency, hence, both are different modes
 //these are the parameter passed to startTx
 #define TX_SSB 0
 #define TX_CW 1
-
-#define DEFAULT_FIRSTIF 45000000L
 
 char ritOn = 0;
 char vfoActive = VFO_A;
